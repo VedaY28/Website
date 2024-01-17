bf6130600228e1b7f6f216e1d27ff66ab30d00d2
VedaY <veda.yakkali3@gmail.com>
2024-01-17 17:44:50 -0500

    Fixed Issue with Nav Bar

--- script.js
+++ script.js
@@ -14,12 +14,13 @@ let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
 
     sections.forEach(sec => {
-        let top = window.screenY;
+        // let top = window.screenY;
+        let top = window.scrollY || document.documentElement.scrollTop;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top <offset + height) {
+        if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
